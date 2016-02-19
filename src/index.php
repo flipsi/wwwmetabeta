d639be9057a0f4efd30b1ecd58cd5a7cbd772451
Philipp Moers <soziflip@gmail.com>
2016-02-19 03:13:38 +0100

    a first design sketch

---
+++ src/index.php
@@ -0,0 +1,103 @@
+<?php
+
+$uiStrings = array(
+    'title' => 'Philipp Moers',
+    'subtitle' => 'sflip.somedomain.com',
+);
+
+
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>
+        <?php echo $uiStrings['title']; ?>
+    </title>
+
+    <meta http-equiv="content-type" content="text/html; charset=utf-8">
+    <meta name="author" content="sflip">
+
+    <!--<script src="//code.jquery.com/jquery-1.12.0.min.js" charset="utf-8"></script>-->
+    <!--<script src="script.js" charset="utf-8"></script>-->
+
+    <link href="style.css" rel="stylesheet" type="text/css" media="screen">
+
+</head>
+<body>
+
+    <header class="header">
+
+        <div class="branding">
+            <a href="">
+            <h1>
+                <?php echo $uiStrings['title']; ?>
+            </h1>
+            <h3>
+                <?php echo $uiStrings['subtitle']; ?>
+            </h3>
+            </a>
+        </div>
+
+        <nav>
+            <ul>
+                <li>
+                    <a href="">About Me</a>
+                </li>
+                <li>
+                    <a href="">Blog</a>
+                </li>
+                <li>
+                    <a href="">Misc</a>
+                </li>
+            </ul>
+        </nav>
+
+    </header>
+
+
+    <div class="pagewrapper">
+        <div class="page">
+
+            <div class="element">
+                <div class="headline">
+                    Sweet
+                </div>
+                <div class="content">
+                    Minions ipsum poopayee poulet tikka masala daa bananaaaa gelatooo. Pepete jeje tulaliloo underweaaar ti aamoo! Para tú pepete daa poulet tikka masala tank yuuu! Poopayee potatoooo bappleees bananaaaa. Underweaaar po kass tank yuuu! Hana dul sae po kass tulaliloo chasy tulaliloo. Po kass poulet tikka masala belloo! Daa ti aamoo! Bappleees pepete bappleees. La bodaaa poulet tikka masala hana dul sae hahaha la bodaaa aaaaaah potatoooo belloo! Jiji tatata bala tu.
+                </div>
+            </div>
+
+            <div class="element">
+                <div class="headline">
+                    Stuff
+                </div>
+                <div class="content">
+                    Tank yuuu! chasy ti aamoo! Para tú poopayee potatoooo uuuhhh. Me want bananaaa! tulaliloo jiji tulaliloo belloo! Chasy hana dul sae gelatooo potatoooo. Daa tatata bala tu bappleees aaaaaah poopayee belloo! Bee do bee do bee do aaaaaah underweaaar me want bananaaa! Poulet tikka masala daa pepete wiiiii. Tatata bala tu jiji poopayee me want bananaaa! Wiiiii.
+                </div>
+            </div>
+
+            <div class="element">
+                <div class="headline">
+                    Gonna
+                </div>
+                <div class="content">
+                    Ti aamoo! para tú me want bananaaa! Wiiiii bappleees para tú butt po kass bee do bee do bee do. Underweaaar gelatooo jeje tatata bala tu tulaliloo bappleees chasy tank yuuu! Poopayee. Bananaaaa jeje baboiii gelatooo poopayee bappleees jiji gelatooo la bodaaa. Daa uuuhhh tank yuuu! Wiiiii butt pepete hahaha. Butt la bodaaa po kass bee do bee do bee do tatata bala tu me want bananaaa! Baboiii poopayee tank yuuu! Pepete wiiiii. Chasy pepete hana dul sae me want bananaaa! Hahaha potatoooo wiiiii bappleees underweaaar. Hana dul sae tank yuuu! Uuuhhh wiiiii hahaha tatata bala tu jeje hahaha. Jiji aaaaaah para tú jiji tulaliloo. Poopayee tatata bala tu jeje belloo! Pepete chasy ti aamoo! Bananaaaa la bodaaa bee do bee do bee do.
+                </div>
+            </div>
+
+            <div class="element">
+                <div class="headline">
+                    Come
+                </div>
+                <div class="content">
+                    Poulet tikka masala wiiiii tank yuuu! Me want bananaaa! Potatoooo jeje me want bananaaa! Po kass ti aamoo! Belloo! Hana dul sae me want bananaaa! Para tú para tú bananaaaa baboiii aaaaaah po kass. Tulaliloo poulet tikka masala aaaaaah jiji jiji jeje poulet tikka masala hahaha bananaaaa. Butt bananaaaa jiji jeje chasy tank yuuu! Butt ti aamoo! Po kass bananaaaa. Butt tank yuuu! Tank yuuu! Belloo! Baboiii bee do bee do bee do tank yuuu! Para tú gelatooo. Ti aamoo! wiiiii para tú hahaha tank yuuu! Hana dul sae po kass me want bananaaa! Para tú underweaaar poulet tikka masala poopayee bananaaaa me want bananaaa! Tatata bala tu wiiiii.
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+
+</body>
