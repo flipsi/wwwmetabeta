b9c639ad006e2814768d6189a548fc6e46c9bac9
Philipp Moers <soziflip@gmail.com>
2016-02-19 14:32:36 +0100

    introduced pages. created aboutme with link icons.

--- src/index.php
+++ src/index.php
@@ -1,8 +1,11 @@
 <?php
 
 $uiStrings = array(
-    'title' => 'Philipp Moers',
+    'title'    => 'Philipp Moers',
     'subtitle' => 'sflip.somedomain.com',
+    'aboutme'  => 'About Me',
+    'blog'     => 'Blog',
+    'misc'     => 'Misc',
 );
 
 
@@ -22,7 +25,9 @@ $uiStrings = array(
     <!--<script src="//code.jquery.com/jquery-1.12.0.min.js" charset="utf-8"></script>-->
     <!--<script src="script.js" charset="utf-8"></script>-->
 
-    <link href="style.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="css/style.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="css/pages/aboutme/aboutme.css" rel="stylesheet" type="text/css" media="screen">
+    <link href="css/pages/blog/blog.css" rel="stylesheet" type="text/css" media="screen">
 
 </head>
 <body>
@@ -30,7 +35,7 @@ $uiStrings = array(
     <header class="header">
 
         <div class="branding">
-            <a href="">
+            <a href="?page=aboutme">
             <h1>
                 <?php echo $uiStrings['title']; ?>
             </h1>
@@ -43,13 +48,19 @@ $uiStrings = array(
         <nav>
             <ul>
                 <li>
-                    <a href="">About Me</a>
+                    <a href="?page=aboutme">
+                        <?php echo $uiStrings['aboutme']; ?>
+                    </a>
                 </li>
                 <li>
-                    <a href="">Blog</a>
+                    <a href="?page=blog">
+                        <?php echo $uiStrings['blog']; ?>
+                    </a>
                 </li>
                 <li>
-                    <a href="">Misc</a>
+                    <a href="?page=misc">
+                        <?php echo $uiStrings['misc']; ?>
+                    </a>
                 </li>
             </ul>
         </nav>
@@ -60,41 +71,30 @@ $uiStrings = array(
     <div class="pagewrapper">
         <div class="page">
 
-            <div class="element">
-                <div class="headline">
-                    Sweet
-                </div>
-                <div class="content">
-                    Minions ipsum poopayee poulet tikka masala daa bananaaaa gelatooo. Pepete jeje tulaliloo underweaaar ti aamoo! Para tú pepete daa poulet tikka masala tank yuuu! Poopayee potatoooo bappleees bananaaaa. Underweaaar po kass tank yuuu! Hana dul sae po kass tulaliloo chasy tulaliloo. Po kass poulet tikka masala belloo! Daa ti aamoo! Bappleees pepete bappleees. La bodaaa poulet tikka masala hana dul sae hahaha la bodaaa aaaaaah potatoooo belloo! Jiji tatata bala tu.
-                </div>
-            </div>
+            <?php
 
-            <div class="element">
-                <div class="headline">
-                    Stuff
-                </div>
-                <div class="content">
-                    Tank yuuu! chasy ti aamoo! Para tú poopayee potatoooo uuuhhh. Me want bananaaa! tulaliloo jiji tulaliloo belloo! Chasy hana dul sae gelatooo potatoooo. Daa tatata bala tu bappleees aaaaaah poopayee belloo! Bee do bee do bee do aaaaaah underweaaar me want bananaaa! Poulet tikka masala daa pepete wiiiii. Tatata bala tu jiji poopayee me want bananaaa! Wiiiii.
-                </div>
-            </div>
+                switch ($_GET['page']) {
 
-            <div class="element">
-                <div class="headline">
-                    Gonna
-                </div>
-                <div class="content">
-                    Ti aamoo! para tú me want bananaaa! Wiiiii bappleees para tú butt po kass bee do bee do bee do. Underweaaar gelatooo jeje tatata bala tu tulaliloo bappleees chasy tank yuuu! Poopayee. Bananaaaa jeje baboiii gelatooo poopayee bappleees jiji gelatooo la bodaaa. Daa uuuhhh tank yuuu! Wiiiii butt pepete hahaha. Butt la bodaaa po kass bee do bee do bee do tatata bala tu me want bananaaa! Baboiii poopayee tank yuuu! Pepete wiiiii. Chasy pepete hana dul sae me want bananaaa! Hahaha potatoooo wiiiii bappleees underweaaar. Hana dul sae tank yuuu! Uuuhhh wiiiii hahaha tatata bala tu jeje hahaha. Jiji aaaaaah para tú jiji tulaliloo. Poopayee tatata bala tu jeje belloo! Pepete chasy ti aamoo! Bananaaaa la bodaaa bee do bee do bee do.
-                </div>
-            </div>
+                    case 'aboutme':
+                        include 'pages/aboutme/aboutme.php';
+                        break;
 
-            <div class="element">
-                <div class="headline">
-                    Come
-                </div>
-                <div class="content">
-                    Poulet tikka masala wiiiii tank yuuu! Me want bananaaa! Potatoooo jeje me want bananaaa! Po kass ti aamoo! Belloo! Hana dul sae me want bananaaa! Para tú para tú bananaaaa baboiii aaaaaah po kass. Tulaliloo poulet tikka masala aaaaaah jiji jiji jeje poulet tikka masala hahaha bananaaaa. Butt bananaaaa jiji jeje chasy tank yuuu! Butt ti aamoo! Po kass bananaaaa. Butt tank yuuu! Tank yuuu! Belloo! Baboiii bee do bee do bee do tank yuuu! Para tú gelatooo. Ti aamoo! wiiiii para tú hahaha tank yuuu! Hana dul sae po kass me want bananaaa! Para tú underweaaar poulet tikka masala poopayee bananaaaa me want bananaaa! Tatata bala tu wiiiii.
-                </div>
-            </div>
+                    case 'blog':
+                        include 'pages/blog/blog.php';
+                        break;
+
+                    case 'misc':
+                        include 'pages/blog/blog.php';
+                        break;
+
+                    default:
+                        include 'pages/aboutme/aboutme.php';
+                        break;
+                }
+
+             ?>
+
+
 
         </div>
     </div>
@@ -105,7 +105,7 @@ $uiStrings = array(
 
         <div class="copyright">
             © Copyright 2016
-            <a href="#">Philipp Moers</a>.
+            <a href="?page=aboutme">Philipp Moers</a>.
             All rights reserved.
         </div>
     </footer>
---
+++ src/pages/aboutme/aboutme.php
@@ -0,0 +1,48 @@
+<div class="aboutme">
+    <img class="portrait" src="img/bewerbungsfoto-20140620.jpg" alt="portait"/>
+    <div class="thatsme">
+        <div class="hi">
+            Hi!
+        </div>
+        I am Philipp.
+        <br>
+        I like computer science, music and chocolate.
+
+        <div class="links">
+            You can find me and get in touch on several places:
+            <ul>
+                <li>
+                    <a href="http://www.facebook.com/soziflip" target="_blank">
+                        <img src="img/icon/facebook/facebook.png" alt="Facebook"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/soziflip" target="_blank">
+                        <img src="img/icon/twitter/twitter.png" alt="Twitter"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCS80KceXi-mHB51YmZmdlSw" target="_blank">
+                        <img src="img/icon/youtube/youtube_icon.png" alt="Youtube"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://github.com/sflip" target="_blank">
+                        <img src="img/icon/github/github.png" alt="GitHub"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="http://stackoverflow.com/users/4568748/philipp-moers?tab=profile" target="_blank">
+                        <img src="img/icon/stackoverflow/stackoverflow.png" alt="Stack Overflow"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:soziflip@gmail.com">
+                        <img src="img/icon/mail/iconarchive.png" alt="Mail"/>
+                    </a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+</div>
