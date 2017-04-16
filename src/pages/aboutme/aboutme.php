7e69a17136a323de97f8cfed8252b65a920c0cda
Philipp Moers <soziflip@gmail.com>
2017-04-16 19:41:37 +0200

    add linkedin. and open links in same tab.

--- src/pages/aboutme/aboutme.php
+++ src/pages/aboutme/aboutme.php
@@ -11,32 +11,37 @@
         <div class="links">
             <ul>
                 <li>
-                    <a href="https://www.xing.com/profile/Philipp_Moers3" target="_blank">
+                    <a href="https://www.linkedin.com/in/sflip/">
+                        <img src="img/icon/linkedin/linkedin.png" alt="LinkedIn"/>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.xing.com/profile/Philipp_Moers3/">
                         <img src="img/icon/xing/xing.png" alt="XING"/>
                     </a>
                 </li>
                 <li>
-                    <a href="http://www.facebook.com/soziflip" target="_blank">
+                    <a href="http://www.facebook.com/soziflip/">
                         <img src="img/icon/facebook/facebook.png" alt="Facebook"/>
                     </a>
                 </li>
                 <li>
-                    <a href="https://twitter.com/soziflip" target="_blank">
+                    <a href="https://twitter.com/soziflip/">
                         <img src="img/icon/twitter/twitter.png" alt="Twitter"/>
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.youtube.com/channel/UCS80KceXi-mHB51YmZmdlSw" target="_blank">
+                    <a href="https://www.youtube.com/channel/UCS80KceXi-mHB51YmZmdlSw/">
                         <img src="img/icon/youtube/youtube_icon.png" alt="Youtube"/>
                     </a>
                 </li>
                 <li>
-                    <a href="https://github.com/sflip" target="_blank">
+                    <a href="https://github.com/sflip/">
                         <img src="img/icon/github/github.png" alt="GitHub"/>
                     </a>
                 </li>
                 <li>
-                    <a href="http://stackoverflow.com/users/4568748/philipp-moers?tab=profile" target="_blank">
+                    <a href="http://stackoverflow.com/users/4568748/philipp-moers?tab=profile/">
                         <img src="img/icon/stackoverflow/stackoverflow.png" alt="Stack Overflow"/>
                     </a>
                 </li>
