da9313cca44cecf7b883ad46e6eacffdafde9eb3
Philipp Moers <soziflip@gmail.com>
2020-03-10 01:26:50 +0100

    Add banana with theuselessweb link

--- src/pages/aboutme/aboutme.php
+++ src/pages/aboutme/aboutme.php
@@ -27,8 +27,8 @@
 
 <div class="aboutme">
     <div class="portrait">
-        <img class="regular" src="img/bewerbungsfoto-20170602-regular.jpg" alt="portait"/>
-        <img class="fun" src="img/bewerbungsfoto-20170602-fun.jpg" alt="portait"/>
+        <img class="regular" src="img/bewerbungsfoto-20170602-regular.jpg" alt="portait" />
+        <img class="fun" src="img/bewerbungsfoto-20170602-fun.jpg" alt="portait" />
     </div>
     <div class="thatsme">
         <div class="hi">
@@ -43,57 +43,62 @@
             <ul>
                 <li>
                     <a href="https://www.linkedin.com/in/sflip/">
-                        <img src="img/icon/iconfinder_11-linkedin_104493.png" alt="LinkedIn"/>
+                        <img src="img/icon/iconfinder_11-linkedin_104493.png" alt="LinkedIn" />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.xing.com/profile/Philipp_Moers3/">
-                        <img src="img/icon/iconfinder_xing_115685.png" alt="XING"/>
+                        <img src="img/icon/iconfinder_xing_115685.png" alt="XING" />
                     </a>
                 </li>
                 <li>
                     <a href="https://twitter.com/soziflip/">
-                        <img src="img/icon/iconfinder_03-twitter_104501.png" alt="Twitter"/>
+                        <img src="img/icon/iconfinder_03-twitter_104501.png" alt="Twitter" />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.instagram.com/soziflip/">
-                        <img src="img/icon/iconfinder_38-instagram_1161953.png" alt="Instagram"/>
+                        <img src="img/icon/iconfinder_38-instagram_1161953.png" alt="Instagram" />
                     </a>
                 </li>
                 <li>
                     <a href="http://www.facebook.com/soziflip/">
-                        <img src="img/icon/iconfinder_icon-social-facebook_211902.png" alt="Facebook"/>
+                        <img src="img/icon/iconfinder_icon-social-facebook_211902.png" alt="Facebook" />
                     </a>
                 </li>
                 <li>
                     <a href="https://www.youtube.com/channel/UCS80KceXi-mHB51YmZmdlSw/">
-                        <img src="img/icon/iconfinder_icon-social-youtube-outline_211928.png" alt="Youtube"/>
+                        <img src="img/icon/iconfinder_icon-social-youtube-outline_211928.png" alt="Youtube" />
                     </a>
                 </li>
                 <li>
                     <a href="https://github.com/sflip/">
-                        <img src="img/icon/iconfinder_icon-social-github_211904.png" alt="GitHub"/>
+                        <img src="img/icon/iconfinder_icon-social-github_211904.png" alt="GitHub" />
                     </a>
                 </li>
                 <li>
                     <a href="http://stackoverflow.com/users/4568748/philipp-moers?tab=profile/">
-                        <img src="img/icon/iconfinder_stackoverflow_394194.png" alt="Stack Overflow"/>
+                        <img src="img/icon/iconfinder_stackoverflow_394194.png" alt="Stack Overflow" />
                     </a>
                 </li>
                 <li>
                     <a href="https://paypal.me/sflip">
-                        <img src="img/icon/iconfinder_paypal-bank-transaction-money-platform_1169164.png " alt="PayPal"/>
+                        <img src="img/icon/iconfinder_paypal-bank-transaction-money-platform_1169164.png " alt="PayPal" />
                     </a>
                 </li>
                 <li>
                     <a href="https://t.me/sflip">
-                        <img src="img/icon/iconfinder_335_Telegram_logo_4375490.png" alt="Telegram"/>
+                        <img src="img/icon/iconfinder_335_Telegram_logo_4375490.png" alt="Telegram" />
                     </a>
                 </li>
                 <li>
                     <a href="mailto:soziflip@gmail.com">
-                        <img src="img/icon/iconfinder_Streamline-58_185078.png" alt="Mail"/>
+                        <img src="img/icon/iconfinder_Streamline-58_185078.png" alt="Mail" />
+                    </a>
+                </li>
+                <li>
+                    <a href="https://theuselessweb.com/">
+                        <img src="img/icon/iconfinder_Fruits_Outline-10_3692816.png" id="banana" alt="Banana" />
                     </a>
                 </li>
             </ul>
