991eb2a4ec725c269319d66e3f89629b044f64b8
Philipp Moers <soziflip@gmail.com>
2020-05-09 12:49:32 +0200

    Extend CV with links

--- src/pages/aboutme/aboutme.php
+++ src/pages/aboutme/aboutme.php
@@ -5,35 +5,46 @@
         <li class="container left">
             <div class="content">
                 <div class="when">May 2017 - Present</div>
-                <div class="where">Ryte GmbH</div>
+                <div class="where">
+                    <a href="https://ryte.com">Ryte GmbH</a>
+                </div>
                 <div class="what">Software Engineer</div>
             </div>
         </li>
         <li class="container right">
             <div class="content">
                 <div class="when">October 2015 - March 2017</div>
-                <div class="where">brainworks computer technologie GmbH</div>
+                <div class="where">
+                    <a href="https://www.brainworks.de/">brainworks computer technologie GmbH</a>
+                </div>
                 <div class="what">Web Developer</div>
             </div>
         </li>
         <li class="container left">
             <div class="content">
                 <div class="when">2014 - 2017</div>
-                <div class="where">Ludwig-Maximilians Universität München</div>
+                <div class="where">
+                    <a href="https://www.uni-muenchen.de/index.html">Ludwig-Maximilians Universität München</a>
+                </div>
                 <div class="what">Master of Science (M.Sc.) Media Informatics</div>
             </div>
         </li>
         <li class="container right">
             <div class="content">
                 <div class="when">2011 - 2014</div>
-                <div class="where">University of Tübingen</div>
+                <div class="where"></div>
+                <div class="where">
+                    <a href="https://uni-tuebingen.de/">University of Tübingen</a>
+                </div>
                 <div class="what">Bacherlor of Science (B.Sc.) Media Informatics</div>
             </div>
         </li>
         <li class="container left">
             <div class="content">
                 <div class="when">2002 - 2011</div>
-                <div class="where">Gymnasium Odenthal</div>
+                <div class="where">
+                    <a href="https://www.gymnasium-odenthal.de/">Gymnasium Odenthal</a>
+                </div>
                 <div class="what">Abitur</div>
             </div>
         </li>
@@ -41,7 +52,7 @@
             <div class="content">
                 <div class="when">1998</div>
                 <div class="where">Schwimmverein Paffrath</div>
-                <div class="what">Seepferdchen</div>
+                <div class="what"><a href="https://de.wikipedia.org/wiki/Fr%C3%BChschwimmer">Seepferdchen</a></div>
             </div>
         </li>
     </ul>
