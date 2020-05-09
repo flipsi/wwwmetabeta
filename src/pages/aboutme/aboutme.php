adcc6b7022f056148dcf7c0d10b871828bd33501
Philipp Moers <soziflip@gmail.com>
2020-05-09 12:34:40 +0200

    Add timeline as curriculum vitae

--- src/pages/aboutme/aboutme.php
+++ src/pages/aboutme/aboutme.php
@@ -1,9 +1,50 @@
 <div class="aboutme">
 
     <h1>Curriculum Vitae</h1>
-    <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
-dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
+    <ul class="curriculumvitae">
+        <li class="container left">
+            <div class="content">
+                <div class="when">May 2017 - Present</div>
+                <div class="where">Ryte GmbH</div>
+                <div class="what">Software Engineer</div>
+            </div>
+        </li>
+        <li class="container right">
+            <div class="content">
+                <div class="when">October 2015 - March 2017</div>
+                <div class="where">brainworks computer technologie GmbH</div>
+                <div class="what">Web Developer</div>
+            </div>
+        </li>
+        <li class="container left">
+            <div class="content">
+                <div class="when">2014 - 2017</div>
+                <div class="where">Ludwig-Maximilians Universität München</div>
+                <div class="what">Master of Science (M.Sc.) Media Informatics</div>
+            </div>
+        </li>
+        <li class="container right">
+            <div class="content">
+                <div class="when">2011 - 2014</div>
+                <div class="where">University of Tübingen</div>
+                <div class="what">Bacherlor of Science (B.Sc.) Media Informatics</div>
+            </div>
+        </li>
+        <li class="container left">
+            <div class="content">
+                <div class="when">2002 - 2011</div>
+                <div class="where">Gymnasium Odenthal</div>
+                <div class="what">Abitur</div>
+            </div>
+        </li>
+        <li class="container right">
+            <div class="content">
+                <div class="when">1998</div>
+                <div class="where">Schwimmverein Paffrath</div>
+                <div class="what">Seepferdchen</div>
+            </div>
+        </li>
+    </ul>
 
     <h1>Skills</h1>
     <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et
