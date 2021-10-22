f1d54b5f698a9e8b0e6045325db87a30e62ad356
Philipp Moers <soziflip@gmail.com>
2021-10-22 17:25:41 +0200

    Add Twitter timeline as blog

--- src/pages/blog/blog.php
+++ src/pages/blog/blog.php
@@ -1,17 +1,18 @@
 <div class="element">
-    <div class="headline">
-        Quite frankly...
-    </div>
-    <div class="content">
-        I don't have a blog yet. This is just a stupid mockup.
-    </div>
-</div>
 
-<div class="element">
+    <!--
     <div class="headline">
-        Soon...
+        Quite frankly, I don't have a blog. Read my twitter timeline instead!
     </div>
-    <div class="content">
-        I'm going to add something interesting here.
+    -->
+
+    <br>
+
+    <div>
+        <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/soziflip?ref_src=twsrc%5Etfw">
+            Tweets by soziflip
+        </a>
+        <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
     </div>
+
 </div>
