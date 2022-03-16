58b8c0e4d9e4c2e7aa3b70c443161b2d07d70396
Philipp Moers <soziflip@gmail.com>
2022-03-16 14:36:04 +0100

    Add books to wishlist

--- src/pages/wishlist/wishlist.php
+++ src/pages/wishlist/wishlist.php
@@ -4,6 +4,18 @@
     <li><?php echo gettext('wishlist:item:boule'); ?></li>
     <li><?php echo gettext('wishlist:item:chess'); ?></li>
     <li><?php echo gettext('wishlist:item:ultrasonic_cleaner'); ?></li>
+    <li><?php echo gettext('wishlist:item:speedcube_4x4x4'); ?></li>
+</ul>
+
+<h1><?php echo gettext('wishlist:h1:books'); ?></h1>
+<p><?php echo gettext('wishlist:books'); ?></p>
+<ul class="wishlist">
+    <li>The Complete Calvin and Hobbes <em class="isbn">(ISBN 1449433251)</em></li>
+    <li>Cryptography Engineering: Design Principles and Practical Applications <em class="isbn">(ISBN 978-0470474242)</em></li>
+    <li>Einführung in die Physik des 20. Jahrhunderts: Relativitätstheorie, Quantenmechanik, Elementarteilchenphysik und Kosmologie <em class="isbn">(ISBN 978-3868942620)</em></li>
+    <li>Flash Boys: Revolte an der Wall Street <em class="isbn">(ISBN 3593501236)</em></li>
+    <li>Geld und Magie. Eine ökonomische Deutung von Goethes Faust <em class="isbn">(ISBN 3938017252)</em></li>
+    <li>Die Selbstorganisation komplexer Systeme - Ergebnisse aus der Werkstatt der Chaostheorie <em class="isbn">(ISBN 3854525036)</em></li>
 </ul>
 
 <h1><?php echo gettext('wishlist:h1:always_nice'); ?></h1>
