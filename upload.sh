233248d9269405424d2961ee98d546763a73ae66
Philipp Moers <soziflip@gmail.com>
2022-12-12 23:40:49 +0100

    Attempt to bypass interactive password prompt

--- upload.sh
+++ upload.sh
@@ -3,17 +3,32 @@
 set -e
 
 
+function require() {
+    if ! command -v $1 &>/dev/null; then
+        echo "Requires $1"
+        exit 1
+    fi
+}
+
 function upload_via_ftp() {
+    require lftp
     PREPEND_COMMANDS="set ftp:ssl-allow no;" # ¯\_(ツ)_/
     APPEND_COMMANDS="exit"
     lftp -e "$PREPEND_COMMANDS mirror -R $SRC $DST; $APPEND_COMMANDS" -u "$USER,$PASS" "$HOST"
 }
 
 function upload_via_ssh() {
+    require rsync
+    require sshpass
+    PASSWORD_FILE=$(mktemp -p /tmp my_litte_secret.XXXXXX) || exit 2
+    chmod 600 "$PASSWORD_FILE"
+    echo -n "$PASS" > "$PASSWORD_FILE"
     # [[ "${SRC}" != */ ]] && SRC="${SRC}/" # append trailing slash if there is none
     [[ "${SRC}" == */ ]] && SRC="${SRC: : -1}" # remove trailing slash if there is one
-    # scp -r "$SRC/." "$USER@$HOST:/$DST"
-    rsync -rL --progress "$SRC/." "$USER@$HOST:$DST"
+    # Avoid password prompt: https://unix.stackexchange.com/a/111534/119362
+    # sshpass -p $(cat "$PASSWORD_FILE") scp -r "$SRC/." "$USER@$HOST:/$DST"
+    sshpass -p $(cat "$PASSWORD_FILE") rsync -rL --progress "$SRC/." "$USER@$HOST:$DST"
+    rm "$PASSWORD_FILE"
 }
 
 
@@ -24,6 +39,7 @@ function main() {
             echo "Uploading to Strato..."
             USER=$(pass strato/flipsi | grep ssh-user | cut -f2 -d' ')
             HOST=$(pass strato/flipsi | grep ssh-host | cut -f2 -d' ')
+            PASS=$(pass strato/flipsi | head -n1 | tr -d '\n')
             SRC="target/"
             DST="www/"
             upload_via_ssh
