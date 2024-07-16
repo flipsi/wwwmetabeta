24a4b378f84b32c8a5b1a6c1a3f0b4c76cfb8249
Philipp Moers <soziflip@gmail.com>
2024-07-16 18:31:12 +0200

    Comment on X/Twitter embedded timeline

--- src/pages/blog/blog.php
+++ src/pages/blog/blog.php
@@ -8,8 +8,22 @@
 
     <br>
 
+    <?php
+    // Official source:
+    // https://publish.twitter.com/?lang=de&query=https%3A%2F%2Fx.com%2Fsoziflip%2F&theme=dark&widget=Timeline
+
+    // With the changes Elon Musk made to Twitter (X), you can no longer see Tweets unless you are logged in.
+    // Therefore, if your browser does not have cached login credentials for Twitter (X), embedded timelines display "Nothing to see here".
+    // https://stackoverflow.com/a/78186868/4568748
+
+    // TODO: Use user lang
+    // TODO: Use user theme
+    ?>
     <div>
-        <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/soziflip?ref_src=twsrc%5Etfw">
+        <a class="twitter-timeline"
+           data-lang="de"
+            data-theme="dark"
+            href="https://x.com/soziflip?ref_src=twsrc%5Etfw">
             Tweets by soziflip
         </a>
         <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
