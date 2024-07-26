9e9cd19da2be8868940c5c04b91c46f623e84ee7
Philipp Moers <soziflip@gmail.com>
2024-07-26 16:41:02 +0200

    Update wishlist books

--- src/pages/wishlist/wishlist.php
+++ src/pages/wishlist/wishlist.php
@@ -13,14 +13,27 @@
 <ul class="wishlist">
     <li>The Complete Calvin and Hobbes <em class="isbn">(ISBN 1449433251)</em></li>
     <li>What If? 2: Additional Serious Scientific Answers to Absurd Hypothetical Questions (English Edition) <em class="isbn">(ISBN 978-0593542903)</em></li>
+    <li>Math Games with Bad Drawings: 75 1/4 Simple, Challenging, Go-Anywhere Games―And Why They Matter <em class="isbn">(ISBN 0762499869)</em></li>
+    <li>I Am a Strange Loop <em class="isbn">(ISBN 978-0-465-03078-1)</em></li>
+    <li>Process and Reality <em class="isbn">(ISBN 978-0029345702)</em></li>
+    <li>Tomorrow, and Tomorrow, and Tomorrow <em class="isbn">(ISBN 978-0593321201)</em></li>
+    <li>Thinking in Bets: Making Smarter Decisions When You Don't Have All the Facts <em class="isbn">(ISBN 978-0735216358)</em></li>
+    <li>Fooled by Randomness: The Hidden Role of Chance in Life and in the Markets <em class="isbn">(ISBN 978-0141031484)</em></li>
+    <li>Twenty Years of A New Kind of Science <em class="isbn">(ISBN 978-1579550493)</em></li>
     <li>EMPOWERED: Ordinary People, Extraordinary Products <em class="isbn">(ISBN 978-1119691297)</em></li>
-    <li>The Socratic Method: A Practitioner’s Handbook <em class="isbn">(ISBN 978-1567926859)</em></li>
+    <li>The Power of Writing It Down <em class="isbn">(ISBN 978-0310359340)</em></li>
+    <li>Quiet: The Power of Introverts in a World That Can't Stop Talking <em class="isbn">(ISBN 978-0141029191)</em></li>
+    <li>The Socratic Method: A Practitioner's Handbook <em class="isbn">(ISBN 978-1567926859)</em></li>
+    <li>Awareness <em class="isbn">(ISBN 978-0006275190)</em></li>
+    <li>Feeling &amp; Knowing: Making Minds Conscious <em class="isbn">(ISBN 978-0525563075)</em></li>
+    <li>The Strange Order of Things: Life, Feeling, and the Making of Cultures <em class="isbn">(ISBN 978-0345807144)</em></li>
     <li>Cryptography Engineering: Design Principles and Practical Applications <em class="isbn">(ISBN 978-0470474242)</em></li>
-    <li>Einführung in die Physik des 20. Jahrhunderts: Relativitätstheorie, Quantenmechanik, Elementarteilchenphysik und Kosmologie <em class="isbn">(ISBN 978-3868942620)</em></li>
-    <li>Flash Boys: Revolte an der Wall Street <em class="isbn">(ISBN 3593501236)</em></li>
-    <li>Geld und Magie. Eine ökonomische Deutung von Goethes Faust <em class="isbn">(ISBN 3938017252)</em></li>
     <li>Die Selbstorganisation komplexer Systeme - Ergebnisse aus der Werkstatt der Chaostheorie <em class="isbn">(ISBN 3854525036)</em></li>
+    <li>Einführung in die Physik des 20. Jahrhunderts: Relativitätstheorie, Quantenmechanik, Elementarteilchenphysik und Kosmologie <em class="isbn">(ISBN 978-3868942620)</em></li>
     <li>Dave Grohl: The Storyteller: Tales of Life and Music <em class="isbn">(ISBN 0063076101)</em></li>
+    <li>Flash Boys: Revolte an der Wall Street <em class="isbn">(ISBN 3593501236)</em></li>
+    <li>Die Pilgerjahre des farblosen Herrn Tazaki <em class="isbn">(ISBN 978-3-8321-9748-3)</em></li>
+    <li>Geld und Magie. Eine ökonomische Deutung von Goethes Faust <em class="isbn">(ISBN 3938017252)</em></li>
 </ul>
 
 <h1><?php echo gettext('wishlist:h1:always_nice'); ?></h1>
