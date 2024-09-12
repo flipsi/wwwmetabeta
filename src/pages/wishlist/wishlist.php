5a7c5268bae499946d50a22cb3629f8b3a182cf6
Philipp Moers <soziflip@gmail.com>
2024-09-12 12:34:55 +0200

    Update wishlist

--- src/pages/wishlist/wishlist.php
+++ src/pages/wishlist/wishlist.php
@@ -2,9 +2,10 @@
 <p><?php echo gettext('wishlist:introduction'); ?></p>
 <ul class="wishlist">
     <li><?php echo gettext('wishlist:item:chess'); ?></li>
+    <li><a href="https://www.diggersfactory.com/vinyl/318915" target="blank"><?php echo gettext('wishlist:item:theo_katzman_vinyl'); ?></a></li>
     <li><a href="https://www.amazon.de/-/en/Coasters-Waterproof-Decorations-Friends-Drinking/dp/B09TQF61C6/ref=sr_1_15?keywords=leuchtende%2Bbierdeckel&qid=1707683312&sr=8-15&th=1" target="blank"><?php echo gettext('wishlist:item:led_beer_mats'); ?></a></li>
-    <li><?php echo gettext('wishlist:item:ultrasonic_cleaner'); ?></li>
     <li><?php echo gettext('wishlist:item:speedcube_4x4x4'); ?></li>
+    <li><?php echo gettext('wishlist:item:ultrasonic_cleaner'); ?></li>
 </ul>
 
 <h1><?php echo gettext('wishlist:h1:books'); ?></h1>
@@ -15,8 +16,6 @@
     <li>Math Games with Bad Drawings: 75 1/4 Simple, Challenging, Go-Anywhere Games―And Why They Matter <em class="isbn">(ISBN 0762499869)</em></li>
     <li>I Am a Strange Loop <em class="isbn">(ISBN 978-0-465-03078-1)</em></li>
     <li>Process and Reality <em class="isbn">(ISBN 978-0029345702)</em></li>
-    <li>Tomorrow, and Tomorrow, and Tomorrow <em class="isbn">(ISBN 978-0593321201)</em></li>
-    <li>Fooled by Randomness: The Hidden Role of Chance in Life and in the Markets <em class="isbn">(ISBN 978-0141031484)</em></li>
     <li>Twenty Years of A New Kind of Science <em class="isbn">(ISBN 978-1579550493)</em></li>
     <li>EMPOWERED: Ordinary People, Extraordinary Products <em class="isbn">(ISBN 978-1119691297)</em></li>
     <li>The Power of Writing It Down <em class="isbn">(ISBN 978-0310359340)</em></li>
